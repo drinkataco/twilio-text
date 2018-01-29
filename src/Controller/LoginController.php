31ae0de94b826ea969c50e9d511b1c32ac34d573
J J Walwyn <me@joshwalwyn.com>
2018-01-29 23:30:14 +0000

    Initialise Doctrine

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,13 +1,37 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Controller for Login / Logout
+ */
 class LoginController extends Controller
 {
-    public function login()
+    /**
+     * Handles Login
+     *
+     * @param Request $request The Request
+     *
+     * @return Response
+     */
+    public function login(Request $request)
     {
-        return $this->render('user/login.html.twig');
+        // $user = new User();
+        // $form = $this->createForm(UserType::class, $user);
+        // $form->handleRequest($request);
+
+        // We want to render the form if GET, otherwise process is
+        if ($request->getMethod() === Request::METHOD_GET) {
+            return $this->render('user/login.html.twig');
+        } else {
+            return new Response('POST ey');
+        }
+    }
+
+    public function logout(Request $request)
+    {
     }
 }
