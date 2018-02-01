85c8ab51028e7a8286d4f08000931bbddd9c5254
J J Walwyn <me@joshwalwyn.com>
2018-02-01 23:49:55 +0000

    Added Redis Cache Adapter

--- src/Controller/Message/MessageController.php
+++ src/Controller/Message/MessageController.php
@@ -20,6 +20,39 @@ class MessageController extends Controller
      */
     public function message(Request $request): Response
     {
+
+        // $redis = new PredisClient();
+
+
+        // create a new item by trying to get it from the cache
+        $cache =  $this->get('cache.app');
+        $numProducts = $cache->getItem('stuff');
+
+        dump($numProducts);
+
+        die();
+
+        // assign a value to the item and save it
+        $numProducts->set(38473);
+        $cache->save($numProducts);
+
+        // retrieve the cache item
+        // $numProducts = $cache->getItem('stats.num_products');
+        // if (!$numProducts->isHit()) {
+        //     // ... item does not exists in the cache
+        // }
+        // retrieve the value stored by the item
+        $total = $numProducts->get();
+
+        // remove the cache item
+        // $cache->deleteItem('stats.num_products');
+        dump($cache->getItems('*'));
+        die();
+
+
+
+
+
         // Build the Message form
         $message = new Message();
         $form = $this->createForm(MessageType::class, $message);
@@ -28,7 +61,15 @@ class MessageController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // Send to REDIS Service
+            // Send to cache service
+            $cache =  $this->get('cache.app');
+
+            $textsToSend = $cache->getItem('texts_to_send');
+
+
+            // Send to Rabbit MQ
+
+
 
             return new Response('this has been sent');
         }
